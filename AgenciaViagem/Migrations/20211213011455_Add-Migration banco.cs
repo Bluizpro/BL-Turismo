ffc24627bbe92d9b8dfc3b1486944f5851172102
bluizpro <bluizprogramador@gmail.com>
2021-12-20 20:20:27 -0300

    seja bem vindo

---
+++ AgenciaViagem/Migrations/20211213011455_Add-Migration banco.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AgenciaViagem.Migrations
+{
+    public partial class AddMigrationbanco : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contatos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CPF = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: true),
+                    Endereco = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Bairro = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cidade = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Telefone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Destino = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nascimento = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contatos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contatos");
+        }
+    }
+}
---
+++ AgenciaViagem/Models/Contato.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgenciaViagem.Models
+{
+    public class Contato
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        [StringLength(11)]
+        public string CPF { get; set; }
+        public string Endereco { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Telefone { get; set; }
+        public string Destino { get; set; }        
+        public DateTime Nascimento { get; set; }
+
+    }
+}
